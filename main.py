f351ef59ae29b29e4943364740b98dc0c8bb23e2
EULERLOSVIN <eulerlosvinmartinezhurtado@gmail.com>
2025-07-07 15:33:31 -0500

    4ta prueba ci/cd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from calculadora import suma, resta, multiplicacion, division
 
-print("Suma:", suma(2, 4))
+print("Suma:", suma(2, 3))
 print("Resta:", resta(10, 4))
 print("Multiplicación:", multiplicacion(5, 6))
 print("División:", division(20, 4))
